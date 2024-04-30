2ae53895d74c4b31c516be9765a70b065926c831
XWJ * <2514034568@qq.com>
2024-05-01 01:48:35 +0800

    LIS3MDL ADS1256 WIFI Transmit in C8T6

--- Python/lis3mdl_esp8266_read.py
+++ Python/lis3mdl_esp8266_read.py
@@ -5,18 +5,19 @@ import matplotlib.pyplot as plt
 
 
 # def read_wifi_data():
-    # 这里放置读取WiFi数据的代码，具体取决于你要获取的数据类型
+# 这里放置读取WiFi数据的代码，具体取决于你要获取的数据类型
 
 # ESP8266的IP地址和端口号
-HOST = '192.168.4.1'  # ESP8266的默认AP IP
-PORT = 333 # 端口号，需要与ESP8266上的服务器端口匹配
+HOST = "192.168.4.1"  # ESP8266的默认AP IP
+PORT = 333  # 端口号，需要与ESP8266上的服务器端口匹配
+
 
 def esp_connected():
     try:
         n = 300
-        data1 = np.zeros(n*6)
-        data2 = np.zeros(n*6)
-        data3 = np.zeros(n*6)
+        data1 = np.zeros(n * 6)
+        data2 = np.zeros(n * 6)
+        data3 = np.zeros(n * 6)
         Bx1 = np.zeros(n)
         By1 = np.zeros(n)
         Bz1 = np.zeros(n)
@@ -27,22 +28,22 @@ def esp_connected():
         By3 = np.zeros(n)
         Bz3 = np.zeros(n)
         while True:
-        # 创建一个socket连接
+            # 创建一个socket连接
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.connect((HOST, PORT))
                 # if not s.recv(1) == b"0x19":
                 #     continue
-                data1 = s.recv(n*6)
-                data2 = s.recv(n*6)
-                data3 = s.recv(n*6)
+                data1 = s.recv(n * 6)
+                data2 = s.recv(n * 6)
+                data3 = s.recv(n * 6)
             #     arr = s.recv(10)
             # for i in range(10):
             #     print(f"arr[{i}]: {arr[i]}\n")
 
             for i in range(n):
-                Bx1[i] = data1[0 + 6*i] << 8 | data1[1 + 6*i]
-                By1[i] = data1[2 + 6*i] << 8 | data1[3 + 6*i]
-                Bz1[i] = data1[4 + 6*i] << 8 | data1[5 + 6*i]
+                Bx1[i] = data1[0 + 6 * i] << 8 | data1[1 + 6 * i]
+                By1[i] = data1[2 + 6 * i] << 8 | data1[3 + 6 * i]
+                Bz1[i] = data1[4 + 6 * i] << 8 | data1[5 + 6 * i]
                 if Bx1[i] > 32767:
                     Bx1[i] -= 65536
                 if By1[i] > 32767:
@@ -55,11 +56,11 @@ def esp_connected():
 
                 # print(f"Bx1: {Bx1[i]}, By1: {By1[i]}, Bz1: {Bz1[i]}\n")
 
-                Bx2[i] = data2[0 + i*6] << 8 | data2[1 + i*6]
-                By2[i] = data2[2 + i*6] << 8 | data2[3 + i*6]
-                Bz2[i] = data2[4 + i*6] << 8 | data2[5 + i*6]
+                Bx2[i] = data2[0 + i * 6] << 8 | data2[1 + i * 6]
+                By2[i] = data2[2 + i * 6] << 8 | data2[3 + i * 6]
+                Bz2[i] = data2[4 + i * 6] << 8 | data2[5 + i * 6]
                 if Bx2[i] > 32767:
-                    Bx2[i] -= 65536    
+                    Bx2[i] -= 65536
                 if By2[i] > 32767:
                     By2[i] -= 65536
                 if Bz2[i] > 32767:
@@ -68,9 +69,9 @@ def esp_connected():
                 By2[i] = By2[i] / 6842
                 Bz2[i] = Bz2[i] / 6842
 
-                Bx3[i] = data3[0 + i*6] << 8 | data3[1 + i*6]
-                By3[i] = data3[2 + i*6] << 8 | data3[3 + i*6]
-                Bz3[i] = data3[4 + i*6] << 8 | data3[5 + i*6]
+                Bx3[i] = data3[0 + i * 6] << 8 | data3[1 + i * 6]
+                By3[i] = data3[2 + i * 6] << 8 | data3[3 + i * 6]
+                Bz3[i] = data3[4 + i * 6] << 8 | data3[5 + i * 6]
                 if Bx3[i] > 32767:
                     Bx3[i] -= 65536
                 if By3[i] > 32767:
@@ -95,23 +96,24 @@ def esp_connected():
     except KeyboardInterrupt:
         print("Program stopped by user")
 
+
 def esp_connected1():
     try:
         group = 3
-        n = 300 #data[group][n]
-        data = np.zeros((group, n*6), dtype=np.uint8)
+        n = 300  # data[group][n]
+        data = np.zeros((group, n * 6), dtype=np.uint8)
         Bx = np.zeros((group, n))
         By = np.zeros((group, n))
         Bz = np.zeros((group, n))
 
         while True:
-        # 创建一个socket连接
+            # 创建一个socket连接
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.connect((HOST, PORT))
 
-                data1 = s.recv(n*6)
-                data2 = s.recv(n*6)
-                data3 = s.recv(n*6)
+                data1 = s.recv(n * 6)
+                data2 = s.recv(n * 6)
+                data3 = s.recv(n * 6)
                 print(f"data1: {data1}\n")
                 # for i in range(group):
                 #     data1 = s.recv(n*6)
@@ -120,8 +122,8 @@ def esp_connected1():
                 # # data[1] = s.recv(n*6)
                 # # data[2] = s.recv(n*6)
                 # print(f"data[0]: {data[0]}\n")
-                
-        #     for i in range(group): 
+
+        #     for i in range(group):
         #         for j in range(n):
         #             Bx[i][j] = data[i][0 + 6*j] << 8 | data[i][1 + 6*j]
         #             By[i][j] = data[i][2 + 6*j] << 8 | data[i][3 + 6*j]
@@ -137,7 +139,6 @@ def esp_connected1():
 
         #     break
 
-
         # Bx = np.concatenate((Bx[0], Bx[1], Bx[2]), axis=1)
         # By = np.concatenate((By[0], By[1], By[2]), axis=1)
         # Bz = np.concatenate((Bz[0], Bz[1], Bz[2]), axis=1)
@@ -147,21 +148,20 @@ def esp_connected1():
         # print(f"length of data1: {len(Bx)}, length of data2: {len(data)}\n")
     except KeyboardInterrupt:
         print("Program stopped by user")
-def esp8266_read():
 
-    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+def esp8266_read():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
-    n= 10
+    n = 10
     rxFlag = s.recv(1)
-    rxFlag = int.from_bytes(rxFlag, byteorder='big')
-    
+    rxFlag = int.from_bytes(rxFlag, byteorder="big")
+
     data = s.recv(n)
     print(f"rxFlag: {rxFlag}\n")
     for i in range(n):
         print(f"data[{i}]: {data[i]}\n")
 
 
-    
 if __name__ == "__main__":
-    
     esp_connected()
--- README.md
+++ README.md
@@ -13,7 +13,24 @@
 
 - PA4 of STM32 is connected to CS of LIS3MDL
 - PA5 of STM32 is connected to SCL of LIS3MDL
-- PA6 of STM32 is connected to SDA of LIS3MDL
-- PA7 of STM32 is connected to SDO of LIS3MDL
+- PA7 of STM32 is connected to SDA of LIS3MDL
+- PA6 of STM32 is connected to SDO of LIS3MDL
 - 3.3V of STM32 is connected to VCC of LIS3MDL
 - SPI1
+
+# 3 LED AND KEY PIN
+
+- LED_ADS1256 PIN =>PC14
+- LED_LIS3MDL PIN =>PC15
+- KEY_ADS1256 PIN =>PB12
+- KEY_LIS3MDL PIN =>PA15
+
+# 4 ADS1256 PIN
+
+- SCLK   => PB13
+- DIN     => PB15
+- DOUT  => PB14
+- DRDY  => PA11
+- CS        => PA8
+- POWN => PA12
+
